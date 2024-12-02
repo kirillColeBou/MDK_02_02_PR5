b696b51809b6b20bbc6d9e3db3a4e56e071c9525
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-02 23:27:25 +0500

    Написан метод ConnectServer. Тепляков

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -95,7 +96,22 @@ namespace Server
             }
         }
 
-
+        static void ConnectServer()
+        {
+            IPEndPoint EndPoint = new IPEndPoint(ServerIPAddress, ServerPort);
+            Socket SocketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketListener.Bind(EndPoint);
+            SocketListener.Listen(MaxClient);
+            while (true)
+            {
+                Socket Handler = SocketListener.Accept();
+                byte[] bytes = new byte[10485760];
+                int byteRec = Handler.Receive(bytes);
+                string Message = Encoding.UTF8.GetString(bytes, 0, byteRec);
+                string Response = SetCommandClient(Message);
+                Handler.Send(Encoding.UTF8.GetBytes(Response));
+            }
+        }
 
         static void GetStatus()
         {
