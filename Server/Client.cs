0f821e36673a9383d8e89c0f16dcda08aa429c82
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-02 23:21:55 +0500

    Реализован класс Client. Тепляков

---
+++ Server/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public DateTime DateConnect { get; set; }
+        public bool IsBlackList { get; set; }
+
+        public Client()
+        {
+            Random rnd = new Random();
+            string Chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            this.Token = new string(Enumerable.Repeat(Chars, 15).Select(x => x[rnd.Next(Chars.Length)]).ToArray());
+            DateConnect = DateTime.Now;
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
