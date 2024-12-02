4506d85495289c1b9a21eb32126e26b0190f4b53
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-02 23:23:25 +0500

    Инициализированы переменные и написан метод Help. Тепляков

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,33 @@ namespace Server
 {
     public class Program
     {
+        static IPAddress ServerIPAddress;
+        static int ServerPort;
+        static int MaxClient;
+        static int Duration;
+        static List<Client> AllClients = new List<Client>();
+
         static void Main(string[] args)
         {
 
+        }
+
+        static void Help()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Command to the clients: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/config");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("  - set initial settings");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/disconnect");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - disconnect users from server");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/status");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("  - show list users");
         }
     }
 }
