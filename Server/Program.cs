ff41161dbe64b4747cd509a197db48f21a1fc2da
kirillColeBou <kirillteplycov888@gmail.com>
2024-12-02 23:24:20 +0500

    Написан метод OnSettings. Тепляков

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -17,7 +18,44 @@ namespace Server
 
         static void Main(string[] args)
         {
+            OnSettings();
 
+        }
+
+        static void OnSettings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            if (File.Exists(Path))
+            {
+                StreamReader sr = new StreamReader(Path);
+                ServerIPAddress = IPAddress.Parse(sr.ReadLine());
+                ServerPort = int.Parse(sr.ReadLine());
+                MaxClient = int.Parse(sr.ReadLine());
+                Duration = int.Parse(sr.ReadLine());
+                sr.Close();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Server IP-address: {ServerIPAddress.ToString()};\nServer port: {ServerPort};\nMax client: {MaxClient};\nDuration: {Duration};");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"Please, provide the IP-address: ");
+                ServerIPAddress = IPAddress.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the port: ");
+                ServerPort = int.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the maximum number of clients: ");
+                MaxClient = int.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the duration of the license: ");
+                Duration = int.Parse(Console.ReadLine());
+                StreamWriter sw = new StreamWriter(Path);
+                sw.WriteLine(ServerIPAddress.ToString());
+                sw.WriteLine(ServerPort);
+                sw.WriteLine(MaxClient);
+                sw.WriteLine(Duration);
+                sw.Close();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("To change, write the command: /config");
         }
 
         static void Help()
