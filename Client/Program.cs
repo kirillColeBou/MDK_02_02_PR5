a9a2f992f60dfea7325349d8546c1b1ac2dba001
Dasha <[persdashka@gmail.com]>
2024-12-03 21:38:31 +0500

    Разработанная функция OnSettings. Ротанова

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client
 {
-    internal class Program
+    class Program
     {
+        static IPAddress ServerIPAddress;
+        static int ServerPort;
+
+        static string ClientToken;
+        static DateTime ClientDateConnection;
+
         static void Main(string[] args)
         {
+            OnSettings();
+        }
+
+        static void OnSettings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            if (File.Exists(Path))
+            {
+                StreamReader sr = new StreamReader(Path);
+                ServerIPAddress = IPAddress.Parse(sr.ReadLine());
+                ServerPort = int.Parse(sr.ReadLine());
+                sr.Close();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Server IP-address: {ServerIPAddress.ToString()}; \nServer port: {ServerPort};");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"Please, provide the IP-address: ");
+                ServerIPAddress = IPAddress.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the port: ");
+                ServerPort = int.Parse(Console.ReadLine());
+                StreamWriter sw = new StreamWriter(Path);
+                sw.WriteLine(ServerIPAddress.ToString());
+                sw.WriteLine(ServerPort);
+                sw.Close();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("To change, write the command: /config");
         }
     }
 }
