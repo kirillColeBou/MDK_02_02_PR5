a72c4c053e26f2c28c43f1b7cb36e88bfc07af8d
Dasha <[persdashka@gmail.com]>
2024-12-03 21:45:23 +0500

    Разработанная функция ConnectServer. Ротанова

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -64,6 +65,42 @@ namespace Client
                 case "/connect": ConnectServer(); break;
                 case "/status": GetStatus(); break;
                 case "/help": Help(); break;
+            }
+        }
+
+        static void ConnectServer()
+        {
+            IPEndPoint EndPoint = new IPEndPoint(ServerIPAddress, ServerPort);
+            Socket Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                Socket.Connect(EndPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            if (Socket.Connected)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Connection to server successful");
+                Socket.Send(Encoding.UTF8.GetBytes("/token"));
+                byte[] bytes = new byte[10485760];
+                int byteRec = Socket.Receive(bytes);
+                string Response = Encoding.UTF8.GetString(bytes, 0, byteRec);
+                if (Response == "/limit")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There isn't enough space on the license server");
+                }
+                else
+                {
+                    ClientToken = Response;
+                    ClientDateConnection = DateTime.Now;
+                    Console.WriteLine($"Received connection token: {ClientToken}");
+                }
             }
         }
     }
